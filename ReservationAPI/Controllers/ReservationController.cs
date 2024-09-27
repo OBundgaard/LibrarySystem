046b6ab6e86c60cc90dc3b86ae1f1d9bbbb2509d
Oliver <Oliverbundgaard@live.dk>
2024-09-27 12:48:03 +0200

    Add project files.

---
+++ ReservationAPI/Controllers/ReservationController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationAPI.Controllers
+{
+    public class ReservationController : Controller
+    {
+        // Dummy data
+        private static List<Reservation> _reservations = new List<Reservation>();
+
+        // Create reservation
+        [HttpPost("{id}")]
+        public IActionResult CreateReservation(int id)
+        {
+            // Reservation must not exist already
+            if (_reservations.Any(r => r.BookID == id))
+                return Conflict(new { message = "Book is already reserved" });
+
+            // Create reservation
+            Reservation reservation = new Reservation 
+            { 
+                BookID = id,
+                ReservedAt = DateTime.Now
+            };
+
+            _reservations.Add(reservation);
+            return Ok(reservation);
+        }
+
+        // Delete reservation
+        [HttpDelete("{id}")]
+        public IActionResult DeleteReservation(int id) 
+        {
+            // Reservation must exist
+            var reservation = _reservations.FirstOrDefault(r => r.BookID == id);
+            if (reservation == null)
+            {
+                return NotFound(new { message = "Reservation not found." });
+            }
+
+            // Delete reservation
+            _reservations.Remove(reservation);
+            return Ok(new { message = "Reservation deleted." });
+        }
+
+    }
+}
---
+++ ReservationAPI/Reservation.cs
@@ -0,0 +1,8 @@
+﻿namespace ReservationAPI
+{
+    public class Reservation
+    {
+        public int BookID { get; set; }
+        public DateTime ReservedAt { get; set; }
+    }
+}
